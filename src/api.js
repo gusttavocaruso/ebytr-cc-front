7b12b100c7340ecd0fda9ad24322f1215a37e0f3
gustavo.caruso <gustavoalmeida26@hotmail.com>
2022-02-17 11:48:33 -0300

    try again

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://ebytr-server.herokuapp.com/' || 'http://localhost:3001'
+  baseURL: 'http://ebytr-server.herokuapp.com/'
 });
 
 export default api; 
